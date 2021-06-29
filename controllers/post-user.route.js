72bd85a84a18a51f7a5c45fb5dedf430a550e44f
daodat2000 <daodat2000@gmail.com>
2021-06-29 16:10:39 +0700

    edit category 2 level and add post

--- controllers/post-user.route.js
+++ controllers/post-user.route.js
@@ -1,21 +1,21 @@
 const express = require("express");
-const productModel = require("../models/product.model");
+const postModel = require("../models/post.model");
 
 const router = express.Router();
 
 router.get("/byCat/:id", async function (req, res) {
-  const catId = +req.params.id || 0;
+  const CatID = +req.params.id || 0;
 
   for (c of res.locals.lcCategories) {
-    if (c.CatID === catId) {
+    if (c.CatID === CatID) {
       c.IsActive = true;
       break;
     }
   }
 
-  // const list = await productModel.findByCatIDLv1(catId);
-  // res.render('vwProducts/byCat', {
-  //   products: list,
+  // const list = await postModel.findByCatIDLv1(CatID);
+  // res.render('vwposts/byCat', {
+  //   posts: list,
   //   empty: list.length === 0
   // });
 
@@ -23,7 +23,7 @@ router.get("/byCat/:id", async function (req, res) {
   const page = req.query.page || 1;
   if (page < 1) page = 1;
 
-  const total = await productModel.countByCatIDLv1(catId);
+  const total = await postModel.countByCatIDLv1(CatID);
   let nPages = Math.floor(total / limit);
   if (total % limit > 0) nPages++;
 
@@ -36,9 +36,9 @@ router.get("/byCat/:id", async function (req, res) {
   }
 
   const offset = (page - 1) * limit;
-  const list = await productModel.findByCatIDLv1(catId, offset);
-  res.render("vwProducts/byCat", {
-    products: list,
+  const list = await postModel.findByCatIDLv1(CatID, offset);
+  res.render("vwposts/byCat", {
+    posts: list,
     empty: list.length === 0,
     page_numbers,
   });
@@ -47,6 +47,8 @@ router.get("/byCat/:id", async function (req, res) {
 router.get("/byCat/:idLv1/:idLv2", async function (req, res) {
   const catIdLv1 = +req.params.idLv1 || 0;
   const catIdLv2 = +req.params.idLv2 || 0;
+  console.log(catIdLv1, catIdLv2);
+
   for (c of res.locals.lcCategories) {
     if (c.CatID === catIdLv1) {
       c.IsActive = true;
@@ -54,9 +56,9 @@ router.get("/byCat/:idLv1/:idLv2", async function (req, res) {
     }
   }
 
-  // const list = await productModel.findByCatIDLv1(catId);
-  // res.render('vwProducts/byCat', {
-  //   products: list,
+  // const list = await postModel.findByCatIDLv1(CatID);
+  // res.render('vwposts/byCat', {
+  //   posts: list,
   //   empty: list.length === 0
   // });
 
@@ -64,7 +66,7 @@ router.get("/byCat/:idLv1/:idLv2", async function (req, res) {
   const page = req.query.page || 1;
   if (page < 1) page = 1;
 
-  const total = await productModel.countByCatIDLv2(catIdLv1, catIdLv2);
+  const total = await postModel.countByCatIDLv2(catIdLv1, catIdLv2);
   let nPages = Math.floor(total / limit);
   if (total % limit > 0) nPages++;
 
@@ -77,31 +79,31 @@ router.get("/byCat/:idLv1/:idLv2", async function (req, res) {
   }
 
   const offset = (page - 1) * limit;
-  const list = await productModel.findByCatIDLv2(catIdLv1, catIdLv2, offset);
-  res.render("vwProducts/byCat", {
-    products: list,
+  const list = await postModel.findByCatIDLv2(catIdLv1, catIdLv2, offset);
+  res.render("vwposts/byCat", {
+    posts: list,
     empty: list.length === 0,
     page_numbers,
   });
 });
 
 router.get("/details/:id", async function (req, res) {
-  const proId = +req.params.id || 0;
+  const CatID = +req.params.id || 0;
 
   // for (c of res.locals.lcCategories) {
-  //   if (c.CatID === catId) {
+  //   if (c.CatID === CatID) {
   //     c.IsActive = true;
   //     break;
   //   }
   // }
 
-  const product = await productModel.findById(proId);
-  if (product === null) {
+  const post = await postModel.findById(CatID);
+  if (post === null) {
     return res.redirect("/");
   }
 
-  res.render("vwProducts/details", {
-    product: product,
+  res.render("vwposts/details", {
+    post: post,
   });
 });
 

7b6250a51585f679a8447c015c0e0fba4c66d66e
baoz <qbao0603@gmail.com>
2021-07-05 11:29:41 +0700

    Edit profile

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,7 @@
 const db = require("../utils/db");
 
 module.exports = {
+<<<<<<< Updated upstream
   all() {
     return db("users");
   },
@@ -30,3 +31,47 @@ module.exports = {
   },
   
 };
+=======
+    all() {
+        return db("users");
+    },
+    getUserByUserName(username) {
+        return db("users").where("username", username).first();
+    },
+    getUserById(ID) {
+        return db("users").where("id", ID).first();
+    },
+    getUserIdByEmail(Email) {
+        return db("users").where("email", Email).first();
+    },
+    getUserByEmail(email) {
+        return db("users").where("email", email).first();
+    },
+    getIdByUIdAndProvider(uid, provider) {
+        return db("linkedusers").where({
+            "uId": uid,
+            "Provider": provider
+        }).first();
+    },
+    addLinkedUser(new_linkedUser) {
+        return db("linkedusers").insert(new_linkedUser);
+    },
+    addUser(new_user) {
+        return db("users").insert(new_user);
+    },
+    updatePremium(id, time) {
+        return db("users")
+            .where("id", id)
+            .update({
+                premium: time,
+            });
+    },
+    patch(new_user) {
+        const ID = new_user.id;
+        delete new_user.id;
+        return db('users')
+            .where('id', ID)
+            .update(new_user)
+    }
+};
+>>>>>>> Stashed changes
